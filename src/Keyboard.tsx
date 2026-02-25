7d4c4b728a86511340b0ba07a9827bf5be779a4d
Jacob <jacobulysseslockwood@gmail.com>
2026-02-25 10:06:51 -0500

    keyboard improvements

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,6 +1,6 @@
 import { createEffect, createSignal, JSX } from "solid-js";
-import { glyphs, ualpha, uomega } from "./glyphs";
-const g = (s: keyof typeof glyphs) => glyphs[s].glyph;
+import { Glyph, glyphs as g } from "./glyphs";
+import { Gly } from "./Highlight";
 
 export type KeyboardControls = {
   highlight(key: string | null): void;
@@ -9,14 +9,21 @@ export type KeyboardControls = {
 export function Keyboard(props: { ref?: (k: KeyboardControls) => void }) {
   const [highlight, setHighlight] = createSignal<string | null>(null);
   const keyMap = new Map<string, string>();
-  keyMap.set(" ", g("_"));
-  function Key(props: { a: string; A: string; b?: string; B?: string }) {
+  keyMap.set(" ", g._.glyph);
+  type GP = Glyph | string;
+  function Key(props: { a: GP; A: GP; b?: GP; B?: GP }) {
+    const toS = (a: GP) => (typeof a === "string" ? a : a.glyph);
     // eslint-disable-next-line solid/reactivity
-    if (props.b) keyMap.set(props.a, props.b);
+    if (props.b) keyMap.set(toS(props.a), toS(props.b));
     // eslint-disable-next-line solid/reactivity
-    if (props.B) keyMap.set(props.A, props.B);
-    // eslint-disable-next-line solid/reactivity
-    const s = props.a + props.A + (props.b ?? "") + (props.B ?? "");
+    if (props.B) keyMap.set(toS(props.A), toS(props.B));
+    const s = // eslint-disable-next-line solid/reactivity
+      toS(props.a) + toS(props.A) + toS(props.b ?? "") + toS(props.B ?? "");
+    const Td = (props: { g: GP | undefined }) => (
+      <td class="h-1/2 w-1/2 text-center align-middle">
+        {typeof props.g === "string" ? props.g : props.g && <Gly g={props.g} />}
+      </td>
+    );
     return (
       <table
         class="h-12 w-12 border-1 border-t-0 border-r-0 border-solid"
@@ -26,12 +33,12 @@ export function Keyboard(props: { ref?: (k: KeyboardControls) => void }) {
       >
         <tbody>
           <tr>
-            <td class="h-1/2 w-1/2 text-center align-middle">{props.A}</td>
-            <td class="h-1/2 w-1/2 text-center align-middle">{props.B}</td>
+            <Td g={props.A} />
+            <Td g={props.B} />
           </tr>
           <tr>
-            <td class="h-1/2 w-1/2 text-center align-middle">{props.a}</td>
-            <td class="h-1/2 w-1/2 text-center align-middle">{props.b}</td>
+            <Td g={props.a} />
+            <Td g={props.b} />
           </tr>
         </tbody>
       </table>
@@ -52,68 +59,68 @@ export function Keyboard(props: { ref?: (k: KeyboardControls) => void }) {
   createEffect(() => props.ref?.({ keyMap, highlight: setHighlight }));
   return (
     <div
-      class="flex flex-col items-center font-mono"
-      classList={{ "text-gray-400": highlight() !== null }}
+      class="flex flex-col items-center font-mono text-green-200/80"
+      classList={{ "!text-gray-400": highlight() !== null }}
     >
       <KeyRow top>
-        <Key a="`" A="~" B={g("emp")} />
-        <Key a="1" A="!" b={g("mn")} B={g("unt")} />
-        <Key a="2" A="@" b={g("dy")} B={g("rep")} />
-        <Key a="3" A="#" b={g("eac")} B={g("#")} />
-        <Key a="4" A="$" b={g("und")} B={g("ctc")} />
-        <Key a="5" A="%" b={g("slf")} B={g("cho")} />
-        <Key a="6" A="^" b={g("bac")} B={g("ari")} />
-        <Key a="7" A="&" b={g("div")} B={g("%")} />
-        <Key a="8" A="*" b={g("mul")} B={g("log")} />
-        <Key a="9" A="(" b={g("inf")} B={g("<<")} />
-        <Key a="0" A=")" b={g("sb")} B={g(">>")} />
-        <Key a="-" A="_" b={g("ng")} B={g("not")} />
-        <Key a="=" A="+" b={g("ne")} B={g("sig")} />
+        <Key a="`" A="~" B={g.emp} />
+        <Key a="1" A="!" b={g.mn} B={g.unt} />
+        <Key a="2" A="@" b={g.dy} B={g.rep} />
+        <Key a="3" A="#" b={g.eac} B={g["#"]} />
+        <Key a="4" A="$" b={g.und} B={g.ctc} />
+        <Key a="5" A="%" b={g.slf} B={g.cho} />
+        <Key a="6" A="^" b={g.bac} B={g.ari} />
+        <Key a="7" A="&" b={g.div} B={g["%"]} />
+        <Key a="8" A={g.pow} b={g.mul} B={g.log} />
+        <Key a="9" A={g["("]} b={g.inf} B={g["<<"]} />
+        <Key a="0" A={g[")"]} b={g.sb} B={g[">>"]} />
+        <Key a={g.sub} A="_" b={g.ng} B={g.not} />
+        <Key a={g.eq} A={g.add} b={g.ne} B={g.sig} />
         <PlaceholderKey class="w-18" />
       </KeyRow>
       <KeyRow>
         <PlaceholderKey class="w-18" />
-        <Key a="q" A="Q" b={g("rev")} B={g("rot")} />
-        <Key a="w" A="W" b={g("w")} B={uomega} />
-        <Key a="e" A="E" b={g("mem")} B={g("rou")} />
-        <Key a="r" A="R" b={g("res")} B={g("sqr")} />
-        <Key a="t" A="T" b={g("tra")} />
-        <Key a="y" A="Y" b={g("tak")} B={g("max")} />
-        <Key a="u" A="U" b={g("dro")} B={g("min")} />
-        <Key a="i" A="I" b={g("iot")} B={g("whe")} />
-        <Key a="o" A="O" b={g("ov")} />
-        <Key a="p" A="P" b={g("pi")} B={g("tau")} />
-        <Key a="[" A="{" b={g(":")} B={g("lft")} />
-        <Key a="]" A="}" b={g("::")} B={g("rgt")} />
-        <Key a="\" A="|" B={g("abs")} />
+        <Key a="q" A="Q" b={g.rev} B={g.rot} />
+        <Key a="w" A="W" b={g.w} B={g.ww} />
+        <Key a="e" A="E" b={g.mem} B={g.rou} />
+        <Key a="r" A="R" b={g.res} B={g.sqr} />
+        <Key a="t" A="T" b={g.tra} />
+        <Key a="y" A="Y" b={g.tak} B={g.max} />
+        <Key a="u" A="U" b={g.dro} B={g.min} />
+        <Key a="i" A="I" b={g.iot} B={g.whe} />
+        <Key a="o" A="O" b={g.ov} />
+        <Key a="p" A="P" b={g.pi} B={g.tau} />
+        <Key a={g["["]} A={g["{"]} b={g[":"]} B={g.lft} />
+        <Key a={g["]"]} A={g["}"]} b={g["::"]} B={g.rgt} />
+        <Key a={g.sca} A={g.mod} B={g.abs} />
       </KeyRow>
       <KeyRow>
         <PlaceholderKey class="w-21" />
-        <Key a="a" A="A" b={g("a")} B={ualpha} />
-        <Key a="s" A="S" b={g("sel")} B={g("pic")} />
-        <Key a="d" A="D" b={g("len")} B={g("sha")} />
-        <Key a="f" A="F" b={g("bef")} />
-        <Key a="g" A="G" b={g("aft")} />
+        <Key a="a" A="A" b={g.a} B={g.aa} />
+        <Key a="s" A="S" b={g.sel} B={g.pic} />
+        <Key a="d" A="D" b={g.len} B={g.sha} />
+        <Key a="f" A="F" b={g.bef} />
+        <Key a="g" A="G" b={g.aft} />
         <Key a="h" A="H" />
         <Key a="j" A="J" />
-        <Key a="k" A="K" b={g("cel")} B={g("rnk")} />
-        <Key a="l" A="L" b={g("&")} B={g("gro")} />
-        <Key a=";" A=":" b={g("exc")} B={g("fmt")} />
-        <Key a="'" A='"' b={g("fla")} B={g("con")} />
+        <Key a="k" A="K" b={g.cel} B={g.rnk} />
+        <Key a="l" A="L" b={g["&"]} B={g.gro} />
+        <Key a=";" A=":" b={g.exc} B={g.fmt} />
+        <Key a="'" A='"' b={g.fla} B={g.con} />
         <PlaceholderKey class="w-21" />
       </KeyRow>
       <KeyRow>
         <PlaceholderKey class="w-27" />
-        <Key a="z" A="Z" b={g("tab")} B={g("win")} />
-        <Key a="x" A="X" b={g("enc")} B={g("par")} />
-        <Key a="c" A="C" b={g("fix")} B={g("cou")} />
-        <Key a="v" A="V" b={g("gru")} B={g("grd")} />
-        <Key a="b" A="B" b={g("flo")} B={g("cei")} />
-        <Key a="n" A="N" b={g("mer")} B={g("fil")} />
-        <Key a="m" A="M" b={g("mat")} B={g("nmt")} />
-        <Key a="," A="<" b={g("cat")} B={g("lte")} />
-        <Key a="." A=">" b={g("id")} B={g("gte")} />
-        <Key a="/" A="?" b={g("fol")} B={g("rpl")} />
+        <Key a="z" A="Z" b={g.tab} B={g.win} />
+        <Key a="x" A="X" b={g.enc} B={g.par} />
+        <Key a="c" A="C" b={g.fix} B={g.cou} />
+        <Key a="v" A="V" b={g.gru} B={g.grd} />
+        <Key a="b" A="B" b={g.flo} B={g.cei} />
+        <Key a="n" A="N" b={g.mer} B={g.fil} />
+        <Key a="m" A="M" b={g.mat} B={g.nmt} />
+        <Key a="," A={g.gt} b={g.cat} B={g.le} />
+        <Key a="." A={g.lt} b={g.id} B={g.ge} />
+        <Key a={g.red} A={g.rol} b={g.fol} B={g.rpl} />
         <PlaceholderKey class="w-27" />
       </KeyRow>
       <KeyRow>
@@ -125,7 +132,9 @@ export function Keyboard(props: { ref?: (k: KeyboardControls) => void }) {
           }}
         >
           <p>space</p>
-          <p class="ml-10">{g("_")}</p>
+          <p class="ml-10">
+            <Gly g={g._} />
+          </p>
         </div>
         <PlaceholderKey class="w-63" />
       </KeyRow>
